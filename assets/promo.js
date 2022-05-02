b6050b44503fec0f895d32b88315560a27b1ab50
Djoa <siongdjoa3@gmail.com>
2022-05-02 16:55:39 +0200

    change var to let

--- assets/promo.js
+++ assets/promo.js
@@ -1,13 +1,13 @@
-var deadline = new Date("may 10, 2022 12:00:00").getTime();
+let deadline = new Date("may 10, 2022 12:00:00").getTime();
 
-var x = setInterval(function() {
+let x = setInterval(function() {
 
-var now = new Date().getTime();
-var t = deadline - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
+let now = new Date().getTime();
+let t = deadline - now;
+let days = Math.floor(t / (1000 * 60 * 60 * 24));
+let hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((t % (1000 * 60)) / 1000);
 document.getElementById("day").innerHTML =days < 10 ? '0' + days:days;
 document.getElementById("hour").innerHTML =hours < 10 ? '0' + hours:hours;
 document.getElementById("minute").innerHTML = minutes < 10 ? '0' + minutes:minutes; 
